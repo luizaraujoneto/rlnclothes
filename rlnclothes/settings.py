3e6aed4c975798185b633fe2816da6ee58a93f67
luizaraujo <luizaraujoneto@gmail.com>
2023-02-27 23:27:55 -0300

    conexão PostgreSQL

--- rlnclothes/settings.py
+++ rlnclothes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-ukidq4m3)%hczc#+(1ed2)h8lhoqzs6325t-6b7a-($$8s+bt%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -74,13 +74,23 @@ WSGI_APPLICATION = "rlnclothes.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "rlnclothesdb",
+        "USER": "rlnclothesuser",
+        "PASSWORD": "rlnclothes",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
